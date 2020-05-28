0d4817d1fc861246fa42de3e9340cd074abb76fe
Evan Crane <ecrane314@gmail.com>
2020-05-27 21:32:03 -0500

    added pressure and humidity

--- temp.py
+++ temp.py
@@ -8,9 +8,11 @@ sense = SenseHat()
 
 temp1 = sense.get_temperature()
 temp2 = sense.get_temperature_from_pressure()
-
+humid = sense.get_humidity()
+pressure = sense.get_pressure()
 
 os.system("vcgencmd measure_temp")
 os.system("vcgencmd measure_volts")
 os.system("vcgencmd measure_clock arm")
-print(str(temp1), str(temp2))
+os.system("vcgencmd get_throttled")
+print(str(temp1), str(temp2), str(humid), str(pressure))
