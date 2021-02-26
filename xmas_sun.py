bec2ad82d4c842869e7a8939968e8706bca2b6e6
Evan Crane <ecrane314@gmail.com>
2021-02-26 07:55:51 -0600

    dumped json to dict

--- xmas_sun.py
+++ xmas_sun.py
@@ -12,17 +12,27 @@ import requests
 # https://requests.readthedocs.io/en/v0.6.2/api/
 import json
 
-def get_sunrise_sunset(lat=30.267238, lng=-97.755202, civil=0, nautical=0):
+def get_solar_data(lat=30.267238, lng=-97.755202, civil=0, nautical=0):
     """Input lattitude and longitude and return sunrise and sunset times"""
     request_body = "https://api.sunrise-sunset.org/json?lat=%f&lng=%f" % (lat, lng)
     print("Request body: " + request_body)
-
     response = requests.get(request_body)
+    
     print(type(response))
+    #print(response)
 
+    print(type(response.content))
     print(response.content)
 
-    return response
+    return response.content
+
+def extract_phase(data):
+    """Extract a particular phase from the JSON API response"""
+    parse = json.loads(data)
+    print(type(parse))
+    #TODO Fix this extraction from dictionary. It's nested
+    print(parse.get("results.sunrise"))
+    #return parse["results.sunrise"]
 
 if __name__ == "__main__":
-    get_sunrise_sunset()
+    extract_phase(get_solar_data())
