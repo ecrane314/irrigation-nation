a5f8f4d842049a5da72fe71a77703df523ef8b22
Evan Crane <ecrane314@gmail.com>
2021-02-27 07:50:04 -0600

    extraction fixed and exceptions implemented

--- xmas_sun.py
+++ xmas_sun.py
@@ -8,31 +8,43 @@ https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
 Evan Crane May 2020
 """
 
+import json
+import sys
 import requests
 # https://requests.readthedocs.io/en/v0.6.2/api/
-import json
 
-def get_solar_data(lat=30.267238, lng=-97.755202, civil=0, nautical=0):
+
+def get_solar_data(lat=30.267238, lng=-97.755202):
     """Input lattitude and longitude and return sunrise and sunset times"""
     request_body = "https://api.sunrise-sunset.org/json?lat=%f&lng=%f" % (lat, lng)
-    print("Request body: " + request_body)
-    response = requests.get(request_body)
-    
-    print(type(response))
-    #print(response)
+    #print("Request body: " + request_body)
 
-    print(type(response.content))
+    response = requests.get(request_body)
+    print(response)
     #print(response.content)
 
     return response.content
 
+
 def extract_phase(phase_data, phase="sunrise"):
-    """Extract a particular phase from the JSON API response"""    
+    """Extract a particular phase from the JSON API response"""
     #load json response to dict with phases
     parse = json.loads(phase_data)
-    
+
+    print("In extract fn: " + phase)
     #print(list(parse))
-    return(parse["results"][phase])
+
+    try:
+        return parse["results"][phase]
+    except KeyError:
+        print("No phase named: " + phase)
+
 
 if __name__ == "__main__":
-    print(extract_phase(get_solar_data()))
+    try:
+        in_phase = sys.argv[1]
+        print("Main try argv: " + in_phase)
+        print(extract_phase(get_solar_data(), phase=in_phase))
+    except AttributeError:
+        print("=== Exception loop! ===")
+        print(extract_phase(get_solar_data()))
