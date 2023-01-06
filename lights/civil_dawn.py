291676b48082f68d44eacd1a2fa39fc31be3db8a
Evan Crane <ecrane314@gmail.com>
2023-01-06 16:27:28 -0600

    fixed output and sent data to log

--- lights/civil_dawn.py
+++ lights/civil_dawn.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 
 """Get sunset and sunrise time for a lattitude and longitude
-For example, 30.267238, -97.755202
-Data are provided via API from https://sunrise-sunset.org/api
+returns civil dawn, which is when sun is 6 degrees below horizon
+location in lat_lng.config
 https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
+
+Data provided via https://sunrise-sunset.org/api
 Evan Crane - Starting May 2020
 """
 
 import json
 import sys
 import logging
-from activate_lights import activate_lights as lights
+
 #TODO import argparse #for lat lng
 from datetime import datetime, timezone
 
 import requests
-# https://requests.readthedocs.io/en/v0.6.2/api/
 
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename="xmas_sun.log")
+logging.basicConfig(filename="civil_dawn.log", level="DEBUG", format="%(asctime)s %(levelname)s %(message)s")
 
 
 def get_solar_data(lat=30.267238, lng=-97.755202):
@@ -28,39 +29,27 @@ def get_solar_data(lat=30.267238, lng=-97.755202):
     # Define request, includes lat and long, formatting off here
     request_body = "https://api.sunrise-sunset.org/json?\
 lat=%f&lng=%f&formatted=0" % (lat, lng)
-    print("Request body: " + request_body)
+    logger.info("Request body: " + request_body)
 
     # Get and return response
     response = requests.get(request_body)
-    print("=======response=========")
-    print(response)
-    print("=======response.content=========")
-    print(response.content)
+    logger.debug(response.content)
+
     return response.content
 
 
 def extract_phase(phase_data, phase="sunset"):
     """Extract a particular phase from the JSON API response"""
     #loads json response to dict with phases
-    parse = json.loads(phase_data)
-    print("extract_phase, phase: " + phase)
+    parsed = json.loads(phase_data)
 
     #return phase
     try:
-        #TODO Add time zone shift for system time.
-        return parse["results"][phase]
+        #TODO return with time zone shift for system time.
+        return parsed["results"][phase]
     except KeyError:
         print("No phase named: " + phase)
         return None
-
-
-def golden_hour()
-    #sunset_tod_in_seconds =    #time of day
-    #gh_begins = sunset_tod_in_seconds - 3600
-    # run lights for 3600 seconds at gh_begins??
-
-def phase_to_seconds()
-    #TODO not sure if needed
 
 
 def utc_to_local(utc_dt):
@@ -85,9 +74,9 @@ def as_timezone(self, timezo):
 if __name__ == "__main__":
     try:
         user_phase = sys.argv[1]
-        #lights(5)
-        print("Main loop argv[1]: " + user_phase)
+        logger.info("Phase Selected: " + user_phase)
         print(extract_phase(get_solar_data(), phase=user_phase))
+        
     except IndexError:
         print("=== Exception loop! ===")
         print(extract_phase(get_solar_data()))
